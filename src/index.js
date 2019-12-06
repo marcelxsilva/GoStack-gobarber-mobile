aeaf242c86a2f50ab2c9b3ef45db4c243934431f
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-12-06 16:06:31 -0300

    configured state redux and saga

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import Routes from './routes';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import { store, persistor } from './store'
 export default function src() {
   return (
-    <>
-      <StatusBar
-        barStyle='dark-content'
-        backgroundColor='#7159c1'
-      />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar
+          barStyle='dark-content'
+          backgroundColor='#7159c1'
+        />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }
---
+++ src/services/api.js
@@ -0,0 +1,2 @@
+import axios from 'axios';
+export default axios.create({ baseURL: 'http://192.168.10.10:333' })
