30e527237ea52c3f5e0e092c8ec5ab1cdded7ce3
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-12-06 09:43:23 -0300

    first commit with project configured

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+
+  settings: {
+    'import/resolver': {
+      "babel-plugin-root-import": {
+        rootPathSuffix: 'src'
+      }
+    }
+  }
+};
