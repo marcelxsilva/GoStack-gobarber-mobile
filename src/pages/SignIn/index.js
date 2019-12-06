b99161f8a5e3b9bc84b78ff6fb43dfdbf2c19c96
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-12-06 10:50:35 -0300

    configured linear gradient and created component background

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 // import { Container } from './styles';
+import Background from '../components/Background';
 
 export default function SignIn() {
   return (
-    <>
+    <Background>
       <Text>SignIn</Text>
-    </>
+    </Background>
   );
 }
