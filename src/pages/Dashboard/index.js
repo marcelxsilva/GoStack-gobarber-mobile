79cf18da1592304a9f57ba595ddfcc0f2a4f87df
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-12-07 15:34:28 -0300

    apply style and list same appointments in dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from "~/components/Background";
-// import { Container } from './styles';
+import Appointments from "~/components/Appointments";
+
+import { Container, Title, List } from './styles';
 
 export default function Dashboard() {
+  const data = [1, 2, 3, 4, 5,6,7,8,10,];
+
   return (
-    <Background />
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Appointments data={item} />}
+        />
+      </Container>
+    </Background>
   );
 }
 
