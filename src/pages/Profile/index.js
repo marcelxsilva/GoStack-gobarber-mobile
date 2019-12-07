2c9a11378de7be3632bf61ceef4a36c073ac9312
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-12-07 16:51:12 -0300

    created style to page profile and apply all funcionalities

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,110 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Background from "~/components/Background";
 import Icon from 'react-native-vector-icons/MaterialIcons';
-// import { Container } from './styles';
+import { Container, Title, Form, FormInput, SubmitButton, Separator } from './styles';
+import { updateProfileResquest } from '~/store/modules/user/actions';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  const emailRef = useRef('');
+  const oldPasswordRef = useRef('');
+  const newPasswordRef = useRef('');
+  const confirmPasswordRef = useRef('');
+
+  const [email, setEmail] = useState(profile.email);
+  const [name, setName] = useState(profile.name);
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewpassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  useEffect(() => {
+    setOldPassword('');
+    setConfirmPassword('');
+    setNewpassword('');
+    
+  }, [profile])
+  async function handleSubmit() {
+    dispatch(updateProfileResquest({
+      name,
+      email,
+      oldPassword,
+      password: newPassword,
+      confirmPassword
+    }));
+
+  }
+
   return (
-    <Background/>
+    <Background>
+      <Container>
+        <Title>Meu Perfil</Title>
+        <Form>
+          <FormInput
+            icon='person-outline'
+            autoCorrect={true}
+            autoCapitalize='none'
+            placeholder='Nome Completo'
+            returnKeyType='next'
+            onSubmitEditing={() => { emailRef.current.focus() }}
+            value={name}
+            onChangeText={setName}
+          />
+
+          <FormInput
+            icon='mail-outline'
+            keyboardType='email-address' // type input
+            autoCorrect={true}
+            autoCapitalize='none'
+            placeholder='E-mail'
+            ref={emailRef}
+            returnKeyType='next'
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <Separator />
+
+          <FormInput
+            icon='lock-outline'
+            secureTextEntry
+            placeholder='Senha Atual'
+            ref={oldPasswordRef}
+            returnKeyType='next'
+            onSubmitEditing={handleSubmit}
+            value={oldPassword}
+            onSubmitEditing={() => { newPasswordRef.current.focus() }}
+            onChangeText={setOldPassword}
+          />
+
+          <FormInput
+            icon='lock-outline'
+            secureTextEntry
+            placeholder='Nova Senha'
+            ref={newPasswordRef}
+            returnKeyType='next'
+            onSubmitEditing={handleSubmit}
+            value={newPassword}
+            onSubmitEditing={() => { confirmPasswordRef.current.focus() }}
+            onChangeText={setNewpassword}
+          />
+
+          <FormInput
+            icon='lock-outline'
+            secureTextEntry
+            placeholder='Confirmação de Senha'
+            ref={confirmPasswordRef}
+            returnKeyType='send'
+            onSubmitEditing={handleSubmit}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+          <SubmitButton onPress={handleSubmit}>Atualizar Perfil</SubmitButton>
+        </Form>
+      </Container>
+    </Background >
   );
 }
 
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -5,19 +5,19 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, avatar_id, ...rest } = payload.data;
+    const { name, email, ...rest } = payload.data;
 
     const profile = Object.assign(
-      { name, email, avatar_id },
+      { name, email },
       rest.oldPassword ? rest : {}
     );
 
     const response = yield call(api.put, 'users', profile);
-    Alert.alert('Sucesso','Perfil atualizado com sucesso!')
+    Alert.alert('Sucesso', 'Perfil atualizado com sucesso!')
     yield put(updateProfileSuccess(response.data));
 
   } catch (err) {
-    Alert.alert('Erro','Confira seus dados!')
+    Alert.alert('Erro', 'Confira seus dados!')
     yield put(updateProfileFailure());
   }
 }
