1885d9ffd992d8b73c405e0e669e1b6fe152805d
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-12-08 16:50:50 -0300

    created all funcionalities to fetch providers from api

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import { Container, ProviderList, Provider, Avatar, Name } from './styles';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+export default function SelectProvider({ navigation }) {
+  const [provider, setProvider] = useState([]);
 
-export default function SelectProvider() {
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+      setProvider(response.data);
+    }
+    loadProviders()
+  }, [])
   return (
     <Background>
-
+      <Container>
+        <ProviderList
+          data={provider}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() => {
+                navigation.navigate('SelectDateTime', { provider })
+              }}>
+              <Avatar source={{ uri: provider.avatar ? provider.avatar.url : `https://api.adorable.io/avatars/50/${provider.name}.png` }} />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
     </Background>
   );
 }
@@ -17,13 +40,13 @@ export default function SelectProvider() {
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
   title: 'Selecione o Prestador',
-    headerLeft: () => (
-      <TouchableOpacity onPress={() => { navigation.navigate('Dashboard') }}>
-        <Icon
-          name='chevron-left'
-          size={30}
-          color='#fff'
-        />
-      </TouchableOpacity>
-    )
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => { navigation.navigate('Dashboard') }}>
+      <Icon
+        name='chevron-left'
+        size={30}
+        color='#fff'
+      />
+    </TouchableOpacity>
+  )
 });
